aec45fe5c9eb979874c3eac86c9cf6b41a071bc3
unknown <yerzhan.rzagaliyev@gmail.com>
2023-11-30 13:48:26 +0500

    initial commit

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -xe
+
+gcc -Wall -Wextra -o twice twice.c -lm
+gcc -Wall -Wextra -o gates gates.c -lm
+gcc -Wall -Wextra -o xor xor.c -lm
---
+++ main.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+float train[][2] = {
+	{0, 0},
+	{1, 2},
+	{2, 4},
+	{3, 6},
+	{4, 8},
+};
+
+#define train_count (sizeof(train)/sizeof(train[0]))
+
+float rand_float() {
+	return (float) rand() / (float) RAND_MAX;
+}
+
+float cost(float w, float b) {
+	float result = 0.0f;
+
+	for (size_t i = 0; i < train_count; ++i) {
+		float x = train[i][0];
+		float y = x * w + b;
+		float d = y - train[i][1];
+		result += d * d;
+	}
+
+	result /= train_count;
+
+	return result; 
+}
+
+int main () {
+	srand(time(0));
+
+	float w = rand_float() * 10.0f;
+	float b = rand_float() * 5.0f;
+
+	float eps = 1e-3;
+	float rate = 1e-3;
+
+	printf("%f\n", cost(w, b));
+
+	for (size_t i = 0; i < 500; ++i) {
+		float dw = (cost(w + eps, b) - cost(w, b)) / eps;
+		float db = (cost(w, b + eps) - cost(w, b)) / eps;
+
+		w -= rate * dw;
+		b -= rate * db;
+
+		printf("cost = %f, w = %f, b = %f\n", cost(w, b), w, b);
+	}
+
+	printf("%f, %f\n", w, b);
+	return 0;
+}
